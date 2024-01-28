26297f5d0ef59f20cfeffad1998091dc9bead626
Брацлавський Захар <100858241+Bratslavskij@users.noreply.github.com>
2024-01-28 12:17:43 +0200

    offer fixed by zahar

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 50,
+  speed: 1000,
+  keyboard: {
+    enabled: true,
+  },
+
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
