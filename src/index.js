799ee43b1f4b49e2611c8f89434f2c7a0e8ea520
Брацлавський Захар <100858241+Bratslavskij@users.noreply.github.com>
2024-02-01 20:59:09 +0200

    header popup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   spaceBetween: 50,
   speed: 1000,
+  keyboard: {
+    enabled: true,
+  },
 
   autoplay: {
     delay: 1000,
@@ -14,3 +17,33 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// (() => {
+//   const mobileMenu = document.querySelector('.mobile-menu');
+//   const openMenuBtn = document.querySelector('.header-popup__close');
+//   const closeMenuBtn = document.querySelector('.header__btn');
+
+//   const toggleMenu = () => {
+//     const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+//       ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
+
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
+
