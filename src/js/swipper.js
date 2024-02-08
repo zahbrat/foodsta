e07b34759cfdf627fcfd761e869d85fe3a8f4d14
Yulchikss <yuliapelia19@gmail.com>
2024-02-08 20:41:35 +0200

    feat: fix mistake and add container styles

--- src/js/swipper.js
+++ src/js/swipper.js
@@ -1,50 +1,48 @@
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 50,
-    speed: 1000,
-    keyboard: {
-      enabled: true,
-    },
-  
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: false,
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
-  // (() => {
-  //   const mobileMenu = document.querySelector('.mobile-menu');
-  //   const openMenuBtn = document.querySelector('.header-popup__close');
-  //   const closeMenuBtn = document.querySelector('.header__btn');
-  
-  //   const toggleMenu = () => {
-  //     const isMenuOpen =
-  //       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-  //     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-  //     mobileMenu.classList.toggle('is-open');
-  
-  //     const scrollLockMethod = !isMenuOpen
-  //       ? 'disableBodyScroll'
-  //       : 'enableBodyScroll';
-  //     bodyScrollLock[scrollLockMethod](document.body);
-  //   };
-  
-  //   openMenuBtn.addEventListener('click', toggleMenu);
-  //   closeMenuBtn.addEventListener('click', toggleMenu);
-  
-  //   // Close the mobile menu on wider screens if the device orientation changes
-  //   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  //     if (!e.matches) return;
-  //     mobileMenu.classList.remove('is-open');
-  //     openMenuBtn.setAttribute('aria-expanded', false);
-  //     bodyScrollLock.enableBodyScroll(document.body);
-  //   });
-  // })();
-  
-  
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 50,
+  speed: 1000,
+  keyboard: {
+    enabled: true,
+  },
+
+  // autoplay: {
+  //   delay: 1000,
+  //   disableOnInteraction: false,
+  // },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// (() => {
+//   const mobileMenu = document.querySelector('.mobile-menu');
+//   const openMenuBtn = document.querySelector('.header-popup__close');
+//   const closeMenuBtn = document.querySelector('.header__btn');
+
+//   const toggleMenu = () => {
+//     const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+//       ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
+
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
