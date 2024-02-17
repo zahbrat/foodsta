6b08fd8ba86617dd99fa6649642afc2ed4ed0aec
Yulchikks <yuliapelia19@gmail.com>
2024-02-17 11:56:42 +0200

    add

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ const swiper = new Swiper('.swiper', {
     enabled: true,
   },
 
-  autoplay: {
-    delay: 1000,
-    disableOnInteraction: false,
-  },
+
 
   navigation: {
     nextEl: '.swiper-button-next',
