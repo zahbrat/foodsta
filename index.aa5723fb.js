46a9fcb1bc37e671715abf6ddfb6c61dc04cb9f3
Yulchikks <yuliapelia19@gmail.com>
2024-02-22 16:58:34 +0200

    Updates

---
+++ index.aa5723fb.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,spaceBetween:50,speed:1e3,keyboard:{enabled:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.aa5723fb.js.map
