a2d4abc2baea8c94deff5013ef3a51f2aab1a54a
YuliaPelia <yuliapelia19@gmail.com>
2024-02-23 13:01:16 +0000

    Deploying to gh-pages from @ zahbrat/foodsta@2241f2619eeb4d6fe3aee6d32bd43af4d47b10f9 🚀

--- index.aa5723fb.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,spaceBetween:50,speed:1e3,keyboard:{enabled:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.aa5723fb.js.map
